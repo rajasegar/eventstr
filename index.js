b82f4a8b871d2a9d7017a1b2ea1812bfbdb6008f
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-03-18 17:16:43 +0530

    chore: add event form

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/eventstr', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Event = require('./models/event');
 
 const app = express();
 
 app.set('view engine','pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const events = await Event.find({});
+
+  res.render('index', { events });
+});
+
+app.post('/events/new', async (req, res) => {
+  console.log(req.body);
+  const newEvent = new Event(req.body);
+  const _doc = await newEvent.save();
+  res.redirect('/');
 });
 
 
